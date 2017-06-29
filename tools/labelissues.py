21453d63e8bd04588d30a458358596b1fc6f3dde
Eric Tsai <md.tsai@gmail.com>
2017-06-29 15:19:33 -0700

    Update with 2 passes

--- tools/labelissues.py
+++ tools/labelissues.py
@@ -20,21 +20,38 @@ from webcompat.db import site_db
 
 
 if __name__ == "__main__":
-    gh = GitHub(token='336f76a547ac6016c5f9075db876caa1e70a6ca3')
+    gh = GitHub(token='35cecb11703f141d78439f6817cac08822627311')
 
     fp = open('web-bugs.txt', 'w')
 
-    for i in range(1, 7840):
+    for i in range(7651, 7840):
         try:
             issue = gh.issue('webcompat', 'web-bugs', i)
             host_name = domain_name(extract_url(issue.body))
+            print str(issue.number) + " " + host_name
             if host_name:
+                found = False
                 for site in site_db.query(Site).filter_by(url=host_name):
                     result = '{} {} {}'.format(issue.number, host_name,
                                                site.priority)
                     fp.write(result + '\n')
                     fp.flush()
-        except:
+                    found = True
+
+                if not found:
+                    subparts = host_name.split('.')
+                    domains = ['.'.join(subparts[i:])
+                               for i, subpart in enumerate(subparts)
+                               if 0 < i < host_name.count('.')]
+                    for domain in domains:
+                        for site in site_db.query(Site).filter_by(url=domain):
+                            result = '{} {} {}'.format(issue.number, domain,
+                                                       site.priority)
+                            fp.write(result + '\n')
+                            fp.flush()
+
+        except Exception, e:
+            print str(e)
             pass
 
         if i % 100 == 0:
